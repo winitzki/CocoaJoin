fa09f73c806c5677643faf7f9bdf7502d74a28f5
Sergei Winitzki <sergei.winitzki@versal.com>
2014-02-14 11:34:38 -0800

    more steps towards new API.

--- DinPhil5/DinPhil5Tests/CJoinTests.m
+++ DinPhil5/DinPhil5Tests/CJoinTests.m
@@ -27,40 +27,45 @@
 }
 
 /// useful for testing an asynchronous operation. Schedule an operation on main thread, then call this, then wait for results. (Otherwise, the main thread will block while waiting for results, because background operations will not be started until the main thread yields control to the runloop.)
-- (void) testExample0 {
+- (void) testExample1 {
     // Here is the full code we would need to write if we don't use any macros.
     
-    // declare some molecule names
+    // create a new join object.
     
+    CJoin *j = [CJoin joinOnMainThread:NO reactionPriority:Default];
     
+    // create new molecule names.
     
-        CJoin *j = [CJoin joinOnMainThread:NO reactionPriority:Default];
-        CjM_empty inc = ^{ [j emptyPutName:@"int"]; };
-        CjM_int counter = ^(int n){ [j intPut:n name:@"counter"]; };
-        CjM_empty_int get = ^int() { return [j empty_intPutName:@"counter"]; };
+    CjM_empty inc = ^{ [j emptyPutName:@"int"]; };
+    CjM_int counter = ^(int value){ [j intPut:value name:@"counter"]; };
+    CjM_empty_int get = ^int() { return [j empty_intPutName:@"counter"]; };
+    
+    // declare reactions using these molecule names.
+    
+    [j defineReactionWithInputNames:@[@"inc", @"counter"] payload:^(NSArray *inputs) {
+        CjR_empty *_cj_inc_M = [inputs objectAtIndex:0];
+        empty dummy = _cj_inc_M.value;
+        (void)dummy;
+        CjR_int *_cj_counter_M = [inputs objectAtIndex:1];
+        int n = _cj_counter_M.value;
+        (void)n;
+        { counter(n+1); } // code given by user in macro
+    } runOnMainThread:NO];
+    
+    [j defineReactionWithInputNames:@[@"counter", @"get"] payload:^(NSArray *inputs) {
+        CjR_int *_cj_counter_M = [inputs objectAtIndex:0];
+        int n = _cj_counter_M.value;
+        (void)n;
+        CjR_empty_int *_cj_get_M = [inputs objectAtIndex:1];
+        empty_int dummy = _cj_get_M.value;
+        (void)dummy;
         
-        // declare reactions using these radicals
-        [j defineReactionWithInputNames:@[@"inc", @"counter"] payload:^(NSArray *inputs) {
-            CjR_empty *_cj_inc_M = [inputs objectAtIndex:0];
-            empty d = _cj_inc_M.value;
-            (void)d;
-            CjR_int *_cj_counter_M = [inputs objectAtIndex:1];
-            int n = _cj_counter_M.value;
-            (void)n;
-            { counter(n+1); } // code given by user in macro
-        } runOnMainThread:NO];
-        
-        [j defineReactionWithInputNames:@[@"counter", @"get"] payload:^(NSArray *inputs) {
-            CjR_int *_cj_counter_M = [inputs objectAtIndex:0];
-            int n = _cj_counter_M.value;
-            (void)n;
-            CjR_empty_int *_cj_get_M = [inputs objectAtIndex:1];
-            empty_int d = _cj_get_M.value;
-            (void)d;
-            
-            counter(n), [CJoin intReply:n to:_cj_get_M];
-        } runOnMainThread:NO];
-        j = nil; // make the variable j unusable now.
+        { counter(n), [CJoin intReply:n to:_cj_get_M]; }
+    } runOnMainThread:NO];
+    
+    j = nil; // make the local variable j unusable now.
+    
+    // now we can inject the molecules and observe the results.
     
     counter(0), inc(), inc();
     [CJoin cycleMainLoopForSeconds:0.2];
@@ -69,70 +74,25 @@
     
     XCTAssertEqual(v, 2, @"async counter increased twice yields 2");
 }
-/*
-- (void)testExample
-{
-    // Here is the full code we would need to write if we don't use any macros.
-    
-    // declare some reagents / radicals
-    CjR_empty *inc;
-    CjR_int *counter;
-    CjR_empty_int *get;
-    {
-        CJoin *j = [CJoin joinOnMainThread:NO reactionPriority:Default];
-        inc = [CjR_empty name:@"inc" join:j];
-        counter = [CjR_int name:@"counter" join:j];
-        
-        // declare reactions using these radicals
-        [j defineReactionWithInputs:@[inc,counter] payload:^(NSArray *inputs) {
-            CjR_empty *incR = [inputs objectAtIndex:0];
-            empty d = incR.value;
-            (void)d;
-            CjR_int *counterR = [inputs objectAtIndex:1];
-            int n = counterR.value;
-            (void)n;
-            { [counter put:n+1]; } // block given by user in macro
-        } runOnMainThread:NO];
-        // second reaction; some molecules can be repeated.
-        counter = [CjR_int name:@"counter" join:j];
-        get = [CjR_empty_int name:@"get" join:j];
-        
-        [j defineReactionWithInputs:@[counter,get] payload:^(NSArray *inputs) {
-            CjR_int *_cj_counter_R = [inputs objectAtIndex:0];
-            int n = _cj_counter_R.value;
-            (void)n;
-            CjR_empty_int *_cj_get_R = [inputs objectAtIndex:1];
-            empty_int d = _cj_get_R.value;
-            (void)d;
-            
-            [counter put:n], [_cj_get_R reply:n];
-        } runOnMainThread:NO];
-        j = nil; // make the variable j unusable now.
-    }
-    [counter put:0], [inc put], [inc put];
-    [self cycleMainLoopForSeconds:0.2];
-    
-    int v = [get put];
-    
-    XCTAssertEqual(v, 2, @"async counter increased twice yields 2");
-}
+
 - (void) testExample2 {
     // same with macros:
     
     cjDef(
           
-          cjAsync(inc, empty)
+          cjAsyncEmpty(inc)
           cjAsync(counter, int)
-          cjSync(int, getValue, empty)
+          cjSyncEmpty(int, getValue)
           
-          cjReact2(inc, empty, _, counter, int, n, [counter put:n+1]; );
-          cjReact2(counter, int, n, getValue, empty_int, _, [counter put:n], cjReply(getValue, n); );
+          cjReact2(inc, empty, dummy, counter, int, n, { counter(n+1); } );
+          cjReact2(counter, int, n, getValue, empty_int, dummy, { counter(n); cjReply(getValue, n); } );
     );
-    [counter put:0], [inc put], [inc put];
-    [self cycleMainLoopForSeconds:0.2];
     
-    int v = [getValue put];
+    counter(0), inc(), inc();
+    [CJoin cycleMainLoopForSeconds:0.2];
+    
+    int v = get();
     
     XCTAssertEqual(v, 2, @"async counter increased twice yields 2");
-}*/
+}
 @end
